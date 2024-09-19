831a924adee89cf0cc7d05272e7c5aa8dd0a984f
srinispice <srini.4047@gmail.com>
2024-09-20 00:42:00 +0530

    functionaloty updared

--- projects/ngx-material-paginator/src/lib/ngx-material-paginator.component.ts
+++ projects/ngx-material-paginator/src/lib/ngx-material-paginator.component.ts
@@ -14,6 +14,7 @@ export class NgxMaterialPaginatorComponent {
   }
 
   totPageCount: number = 0;
+  loopBtnArr: any = [];
 
   @Input({ required: true })
   set configData(val: any) {
@@ -22,7 +23,45 @@ export class NgxMaterialPaginatorComponent {
   }
 
   ngOnInit() {
-    console.log(this.totPageCount);
+    // console.log(this.totPageCount);
+    this.nmpCreatePageButton(10);
+  }
+
+  nmpCreatePageButton(curPage: number) {
+    let btnArr = [];
+    const curBtnArr = {
+      pageNum: curPage,
+      activeCls: true
+    };
+
+    const btnLoop = (curPageNum: number) => {
+      const beforeBtnArr = [];
+      const afterBtnArr = [];
+      for (let i = (curPageNum - 1); i >= (curPageNum - 2); i--) {
+        if (i > 0) {
+          beforeBtnArr.push({
+            pageNum: i,
+            activeCls: false
+          });
+        }
+      }
+
+      for (let i = (curPageNum + 1); i <= (curPageNum + 2); i++) {
+        if (i <= this.totPageCount) {
+          afterBtnArr.push({
+            pageNum: i,
+            activeCls: false
+          });
+        }
+      }
+      return btnArr = [...beforeBtnArr.reverse(), curBtnArr, ...afterBtnArr];
+    }
+    this.loopBtnArr = btnLoop(curPage);
+    console.log(this.loopBtnArr);
+  }
+
+  nmploadPage(pageNum: number) {
+    this.nmpCreatePageButton(pageNum);
   }
 
 }
--- projects/ngx-material-paginator/src/lib/ngx-material-paginator.module.ts
+++ projects/ngx-material-paginator/src/lib/ngx-material-paginator.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NgxMaterialPaginatorComponent } from './ngx-material-paginator.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
@@ -11,7 +12,10 @@ import { MatButtonModule } from '@angular/material/button';
     NgxMaterialPaginatorComponent
   ],
   imports: [
-    MatButtonModule, MatDividerModule, MatIconModule
+    CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
   ],
   exports: [
     NgxMaterialPaginatorComponent
